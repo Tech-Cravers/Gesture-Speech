cd05238f7a20d5d7040d5c4d45e939c3bfde39c9
Rushil Verma <44629500+RushilVerma@users.noreply.github.com>
2021-02-19 10:59:09 +0530

    trainingCNN code is complete with  a Value error in validation_split in fit

--- trainingCNN.py
+++ trainingCNN.py
@@ -1,25 +1,24 @@
-from keras.models import Sequential 
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle
+import numpy as np
 
-X = pickle.load(open("X.pickle","rb"))
+x =np.array(pickle.load(open("X.pickle","rb")))
 y = pickle.load(open("y.pickle","rb"))
 
-X=X/255.0
+x=x/255.0
 
 model = Sequential()
-model.add(    Conv2D(64, (3,3), input_shape=X.shape[1:])    )
-model.add(Activation("relu"))
-model.add(MaxPooling2D(poo_size=(2,2)))
+model.add(Conv2D(64, (3,3), input_shape=x.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(64,(3,3))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(poo_size=(2,2)))
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
 
+model.add(Flatten())
 model.add(Dense(64))
 
 model.add(Dense(1))
@@ -28,4 +27,5 @@ model.add(Activation('sigmoid'))
 model.compile(loss="binary_crossentropy",
              optimizer="adam",
              metrics=['accuracy'])
-model.fit(X, y, batch_size=32, epochs=3, validation_spit=0.1)
+
+model.fit(x, y, batch_size=32, epochs=10, validation_split=0.1)
