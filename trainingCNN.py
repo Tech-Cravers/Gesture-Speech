c8e36094cc34ee8188ce47c50692f8e18224ed15
Rushil Verma <44629500+RushilVerma@users.noreply.github.com>
2021-03-02 21:58:03 +0530

    tried hit end trial on neuron but batch_size=4 for sure

--- trainingCNN.py
+++ trainingCNN.py
@@ -8,25 +8,29 @@ x = pickle.load(open("x.pickle","rb"))
 y = pickle.load(open("y.pickle","rb"))
 
 x=x/255.0
+for i in range(0,1):
+    model = Sequential() #a sequential cnn model to create
 
-model = Sequential()
-model.add(Conv2D(64, (3,3), input_shape=x.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+    #added a neuron to network
+    model.add(Conv2D(64, (3,3), input_shape=x.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    print("size of kernel : "+str(i))
+    #tring hit and trial to find best network to work 
 
-model.add(Conv2D(64,(3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(64,(3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
-model.add(Dense(64))
+    #flattening image data
+    model.add(Flatten())
+    model.add(Dense(64))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))#activation function
 
-model.compile(loss="binary_crossentropy",
-             optimizer="adam",
-             metrics=['accuracy'])
+    model.compile(loss="binary_crossentropy",
+                 optimizer="adam",
+                 metrics=['accuracy'])
 
-model.fit(x, y, batch_size=3, epochs=3, validation_split=0.05) # change parameters to increase accuracy of data
-
+    model.fit(x, y, batch_size=4, epochs=1, validation_split=0.1) # change parameters to increase accuracy of data
