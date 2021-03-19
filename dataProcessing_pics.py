72ce3bd3c9588580f4d3e27acba8c717d9373b61
Rushil Verma <44629500+RushilVerma@users.noreply.github.com>
2021-03-19 11:19:17 +0530

    thresholding is revised in processing

--- dataProcessing_pics.py
+++ dataProcessing_pics.py
@@ -6,6 +6,7 @@ import os
 import cv2
 import scipy.ndimage as sci
 
+
 #custom function to crop the picture to symmetric size of 700x700 for 720x1280 input resolution
 def cropIt(gray,top=10,left=290,right=290,down=10):
     w, h = gray.shape
@@ -13,7 +14,7 @@ def cropIt(gray,top=10,left=290,right=290,down=10):
     return croped_image
 
 #to normalize the images to same no. of pixels
-def resizeIt(img,size=100,median=8):
+def resizeIt(img,size=200,median=8):
     img=np.float32(img)
     r,c=img.shape
     #filtering then resizing image
@@ -21,6 +22,12 @@ def resizeIt(img,size=100,median=8):
     filtered_img=sci.median_filter(resized_img,median)
     return np.uint8(filtered_img)
 
+def preprocessing(img0):
+    IMG_SIZE=200
+    img_resized=resizeIt(img0,IMG_SIZE,5) # resize to normalize data size
+    #ret,imgTh0=cv2.threshold(img_resized, 20, 255,cv2.THRESH_BINARY)
+    imgTh=cv2.adaptiveThreshold(img_resized,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,5)
+    return imgTh
 
 #choose the directory u want to process in which video data is present 
 # videos must be named after the small case letter, it represents in gesture of hand
@@ -31,12 +38,12 @@ alpha = 'a'
 for i in range(0, 26): 
     ALPHABET.append(alpha) 
     alpha = chr(ord(alpha) + 1)
-#print(ALPHABET) # initialized it
 
 #contains the data set to be extracted
 training_data=[] # [ feature , label ]format 
 print(ALPHABET)
 
+IMG_SIZE=200
 #to iterate over every alphabet
 for category in ALPHABET:
     path = os.path.join(DATADIR,category)  # create path to directory
@@ -44,15 +51,12 @@ for category in ALPHABET:
     for img_path in os.listdir(path):  # iterate over each image 
         print(img_path)
         img0 = cv2.imread(os.path.join(path,img_path) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-        IMG_SIZE=200
-        img_resized=resizeIt(img0,IMG_SIZE,5) # resize to normalize data size
-        ret,imgTh0=cv2.threshold(img_resized, 20, 255,cv2.THRESH_BINARY)
-        imgTh=cv2.adaptiveThreshold(img_resized,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,5)
-        cv2.imshow("input",imgTh)
+        img_processed=preprocessing(img0)
+        cv2.imshow("input",img_processed)
         cv2.waitKey(1)
 
         class_num =ALPHABET.index(category)
-        training_data.append([imgTh, class_num])  # add image and classification to our training_data
+        training_data.append([img_processed, class_num])  # add image and classification to our training_data
     
         if cv2.waitKey(1) & 0xFF == ord('q'):#break ongoing process by Q
             break
@@ -78,14 +82,14 @@ y = np.array(y)
 #Let's save this data, so that we don't need to keep calculating it every time we want to play with the neural network model:
 import pickle
 
-pickle_out = open("x.pickle","wb")
+pickle_out = open("xblack_raw.pickle","wb")
 pickle.dump(x, pickle_out)
 pickle_out.close()
 
-pickle_out = open("y.pickle","wb")
+pickle_out = open("yblack_raw.pickle","wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
-print('Pickle file created successfully named <X.pickle> and <Y.pickle> !!!')
+print('Pickle file created successfully if named <X.pickle> and <Y.pickle> !!!')
 print('We can always load it in to our current script, or a totally new one by doing:')
 print('pickle_in = open("X.pickle","rb")')
 print('X = pickle.load(pickle_in)')
