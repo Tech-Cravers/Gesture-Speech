40928bff2128c62b9e0f62d406b378b504c62cbb
Rushil Verma <44629500+RushilVerma@users.noreply.github.com>
2021-03-27 11:55:05 +0530

    revived some testing files

---
+++ testing_model.py
@@ -0,0 +1,77 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+import scipy.ndimage as sci
+import time
+
+# This module is imported so that we can  
+# play the converted audio 
+import os
+
+#to normalize the images to same no. of pixels
+def resizeIt(img,size=100,median=2):
+    img=np.float32(img)
+    r,c=img.shape
+    #filtering then resizing image
+    resized_img=cv2.resize(img,(size,size))
+    filtered_img=sci.median_filter(resized_img,median)
+    return np.uint8(filtered_img)
+
+def preprocessing(img0,IMG_SIZE=100):
+    img_resized=resizeIt(img0,IMG_SIZE,1) # resize to normalize data size
+    #cv2.imshow("intermidieate",img_resized)
+    img_blur = cv2.GaussianBlur(img_resized,(5,5),0)
+    ret,img_th = cv2.threshold(img_blur,0,255,cv2.THRESH_TOZERO+cv2.THRESH_OTSU)
+    imgTh=cv2.adaptiveThreshold(img_th,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,10)
+    #edges = cv2.Canny(img_resized,170, 300)
+    return imgTh
+
+ALPHABET = [] #array containing letters to categorize 
+alpha = 'a'
+for i in range(0, 27): 
+    ALPHABET.append(alpha) 
+    alpha = chr(ord(alpha) + 1)
+
+prev=""
+model = tf.keras.models.load_model("model_name.model")
+cap = cv2.VideoCapture(0) #to load video file 
+prev_time = time.time()
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret==False:
+        break
+    # Our operations on the frame come here
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    IMG_SIZE = 200
+    #print(img_gray.shape)
+    w, h = img_gray.shape
+    top=180
+    left=h-195
+    down=w-50
+    right=195
+    img_rect = cv2.rectangle(img_gray,(right,top),(left,down),(255,0,0),2)
+    img_gray = img_gray[top:down, right:left]
+    #print(img_gray.shape)
+    img_test = preprocessing(img_gray,IMG_SIZE)
+
+    cv2.imshow('whole input frame', np.uint8(img_rect))
+    cv2.imshow('qwerty',img_test)
+    # Display the resulting frame
+    #cv2.imshow('testing this',np.uint8(img_test))
+    
+    prediction = model.predict([img_test.reshape(-1, IMG_SIZE, IMG_SIZE, 1)])
+    #print(img_test)
+    
+    text = ALPHABET[int(np.argmax(prediction[0]))]
+    print('Alphabet: '+text+' Time Required: '+str(time.time()-prev_time))
+    prev_time = time.time()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
